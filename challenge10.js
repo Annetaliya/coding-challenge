c695f195388210ca4d7190198dad199e860680da
Annette <annetaliya@gmail.com>
2025-02-27 12:38:48 -0800

    find sum of target

--- challenge10.js
+++ challenge10.js
@@ -9,4 +9,21 @@
  * may not use the same element twice.
  *
  * You can return the answer in any order.
+ * Input: numbers= [10,20,10,40,50,60,70], target=50
+Output: 2, 3
  */
+
+function findSumIndex(array, target) {
+    const indices = []
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === target) {
+                indices.push(array.indexOf(array[i]))
+            }
+        }
+    }
+    return indices;
+    
+}
+
+console.log(findSumIndex([10,20,10,40,50,60,70], 50))
