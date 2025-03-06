f6319bab0b0080607bb26e772b6a3b7625bd6837
Annette <annetaliya@gmail.com>
2025-03-06 09:24:52 -0800

    truncate string

---
+++ challenge12.js
@@ -0,0 +1,21 @@
+/* Write a JavaScript function to extract a specified number of characters from a string.
+
+Test Data :
+
+console.log(truncate_string("Robin Singh",4));
+"Robi"*/
+
+function truncate_string(str,num) {
+    if (str.length === 0) {
+        return null
+    }
+
+    if (typeof str !== 'string') {
+        return 'Invaid Input'
+    }
+
+    
+    const newStr = str.slice(0, num);
+    return newStr
+}
+console.log(truncate_string("Robin Singh",4))
