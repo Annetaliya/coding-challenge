04a76d844b236d78bbd1786700246ec28023da39
Annette <annetaliya@gmail.com>
2025-03-09 22:28:52 -0700

    camelize string

--- challenge15.js
+++ challenge15.js
@@ -5,6 +5,20 @@ That is: removes all dashes, each word after dash becomes uppercased
 camelize("background-color") == 'backgroundColor';
 .*/
 
-function camelize() {
+function camelize(str) {
+    if (str.length === 0) {
+        return null
+    }
+    if (typeof str !== 'string') {
+        return 'Invalid Input'
+    }
+    const newStr = str.split('-');
+    const result = newStr.map((element, index) => {
+       return index === 0 ? element : element[0].toUpperCase() + element.slice(1)
+    })
+    return result.join('')
+   
     
+   
 }
+console.log(camelize("list-style-image"))
