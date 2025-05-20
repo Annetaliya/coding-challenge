7100c33d7f8ba1f6d86cd39c1a6cc757d29b54a9
Annette <annetaliya@gmail.com>
2025-05-20 11:27:37 -0700

    find the maximum occurence

---
+++ challenge25.js
@@ -0,0 +1,43 @@
+/* 
+Write a JavaScript program to find the most frequent item in an array.
+
+Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+Sample Output : a ( 5 times )
+*/
+
+
+function commonOccurence(arr) {
+    const obj = {};
+    let keys = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]]) {
+            obj[arr[i]] += 1;
+        } else {
+            obj[arr[i]] = 1;
+        }
+    }
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)){
+            keys.push(key)
+        }
+        
+    }
+
+    let max = keys[0];
+    for (let i = 1; i < keys.length; i++){
+        if (keys[i] > max) {
+            max = keys[i]
+        }
+    }
+    for (let key in obj) {
+        if (obj[key] === max) {
+            return `${key} (${max} times)`
+        }
+    }
+    return obj
+}
+
+console.log(commonOccurence([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
