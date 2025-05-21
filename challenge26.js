8c15db802cc1ee926fd48767dbbd7e3f19a33029
Annette <annetaliya@gmail.com>
2025-05-21 12:39:34 -0700

    ordinal numbers

---
+++ challenge26.js
@@ -0,0 +1,37 @@
+/* 
+color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+o = ["th","st","nd","rd"]
+
+Write a JavaScript program to display the colors in the following way :
+
+"1st choice is Blue ."
+"2nd choice is Green."
+"3rd choice is Red."
+- - - - - - - - - - - - -
+
+Note : Use ordinal numbers to tell their position.
+
+ */
+
+function displayOrdinalNumbers(arr) {
+    let suffix;
+    let position;
+    for (let i = 0; i < arr.length; i++) {
+        position = i + 1;
+        if (position % 100 >= 11 && position % 100 <=13) {
+            suffix = 'th'
+        }
+
+        if (position % 10 === 1) {
+            suffix = 'st'
+        } else if (position % 10 === 2) {
+            suffix = 'nd'
+        } else if (position % 10 === 3) {
+            suffix = 'rd'
+        } else {
+            suffix = 'th'
+        }
+        console.log(`${position}${suffix} choice is ${arr[i]}`)
+    }
+}
+displayOrdinalNumbers(["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "])
