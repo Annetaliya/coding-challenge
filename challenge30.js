257a2a05710c363f4dc0082514e5b431839b9310
Annette <annetaliya@gmail.com>
2025-05-26 13:06:29 -0700

    sum of two arrays

---
+++ challenge30.js
@@ -0,0 +1,24 @@
+/* 
+There are two arrays with individual values. Write a JavaScript program to compute 
+the sum of each individual index value in the given array.
+
+Sample array :
+
+array1 = [1,0,2,3,4];
+array2 = [3,5,6,7,8,13];
+
+Expected Output :
+
+[4, 5, 8, 10, 12, 13]
+*/
+
+function findSumOfIndex(array1, array2) {
+    const len = Math.max(array1.length, array2.length)
+    let result= []
+    for (let i = 0; i < len; i++) {
+        let sum = (array1[i] || 0) + (array2[i] || 0)
+        result.push(sum)
+    }
+    return result;
+}
+console.log(findSumOfIndex([1,0,2,3,4],[3,5,6,7,8,13] ))
