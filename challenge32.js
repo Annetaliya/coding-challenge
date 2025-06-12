6d377d01e67de7f1d645abf05d66bc97333c8056
Annette <annetaliya@gmail.com>
2025-06-12 09:45:27 -0700

    find difference between two arrays

--- challenge32.js
+++ challenge32.js
@@ -23,4 +23,21 @@ function difference(arr1, arr2) {
     return result
 
 }
-console.log(difference([1, 2, 3], [100, 2, 1, 10]))
+
+function findDifference(arr1, arr2) {
+    let result = []
+    for (let i = 0; i < arr1.length; i++) {
+        if (!arr2.includes(arr1[i])) {
+            result.push(arr1[i])
+        }
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        if (!arr1.includes(arr2[i])) {
+            result.push(arr2[i])
+        }
+    }
+    return result;
+}
+//console.log(difference([1, 2, 3], [100, 2, 1, 10]))
+console.log(findDifference([1, 2, 3], [100, 2, 1, 10]))
