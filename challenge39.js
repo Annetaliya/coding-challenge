a127c4c14b860cf2a5f770e04aca9591738c6c4f
Annette <annetaliya@gmail.com>
2025-07-01 10:09:19 -0700

    find second largest element using sor method

---
+++ challenge39.js
@@ -0,0 +1,15 @@
+//Javascript program to find the second largest element in an arrayusing sorting
+
+function secondLargest(arr) {
+    function toSort(a,b) {
+        return a-b
+    }
+    arr.sort(toSort)
+    const n = arr.length;
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     return arr[i]
+    // }
+    return arr[n-2]
+} 
+console.log(secondLargest([10,7,1,3]))
