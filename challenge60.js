9f9410fb4b15f6d1fe0010e12f7bfdb6fce1922e
Annette <annetaliya@gmail.com>
2025-08-20 09:49:52 -0700

    find the wave pattern in an array

---
+++ challenge60.js
@@ -0,0 +1,27 @@
+/*
+Given a sorted array arr[] of integers (in ascending order),
+ rearrange the elements in-place to form a wave-like array.
+An array is said to be in wave form if it satisfies the following pattern:
+ arr[0] ≥ arr[1] ≤ arr[2] ≥ arr[3] ≤ arr[4] ≥ ...
+In other words, every even-indexed element should be greater than 
+or equal to its adjacent odd-indexed elements (if they exist).
+
+Note: The given array is sorted in ascending order, and modify 
+the given array in-place without returning a new array.
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [2, 1, 4, 3, 5]
+Explanation: Array elements after sorting it in the waveform are 2, 1, 4, 3, 5.
+ */
+
+function waveArray(arr) {
+    const n = arr.length;
+
+    for(let i = 0; i < n - 1; i+=2) {
+        let temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+    }
+    return arr;
+}
+console.log(waveArray([1, 2, 3, 4, 5]))
