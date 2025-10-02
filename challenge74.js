5efde3c1fe3bcdc04657c3c56758c4c72f87af64
Annette <annetaliya@gmail.com>
2025-10-02 13:24:47 -0700

    find the maximum product of a triplet in an array

---
+++ challenge74.js
@@ -0,0 +1,26 @@
+/**
+ * Given an integer array, find a maximum product of a triplet in the array.
+ * nput:  arr[ ] = [10, 3, 5, 6, 20]
+Output: 1200
+Explanation: Multiplication of 10, 6 and 20
+
+Input:  arr[ ] =  [-10, -3, -5, -6, -20]
+Output: -90
+
+Input: arr[ ] =  [1, -4, 3, -6, 7, 0]
+Output: 168
+ */
+
+function maxProductOfTriplets(arr) {
+    const n = arr.length;
+    function toSort(a,b) {
+        return a-b;
+    }
+
+    arr.sort(toSort)
+
+    let max = Math.max(arr[0] * arr[1] * arr[n-1], arr[n-3] * arr[n-2] * arr[n-1])
+    return max;
+}
+
+console.log(maxProductOfTriplets([1, -4, 3, -6, 7, 0]))
