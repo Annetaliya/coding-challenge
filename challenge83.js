f3b2f3a9b772a4070b42c1aa81ff30471b44dd58
Annette <annetaliya@gmail.com>
2025-11-14 05:42:49 -0300

    get the first n elements in an array

---
+++ challenge83.js
@@ -0,0 +1,43 @@
+/**
+ * Write a JavaScript function to get the first element of an array.
+ *  Passing the parameter 'n' will return the first 'n' elements of the array.
+
+Test Data :
+
+console.log(first([7, 9, 0, -2]));
+
+console.log(first([],3));
+
+console.log(first([7, 9, 0, -2],3));
+
+console.log(first([7, 9, 0, -2],6));
+
+console.log(first([7, 9, 0, -2],-3));
+Expected Output :
+
+7
+[]
+[7, 9, 0]
+[7, 9, 0, -2]
+[]
+
+ */
+
+function first(arr, n) {
+    let result = [];
+
+    if (!n) {
+        return arr[0]
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == n) {
+            break;
+        }
+        result.push(arr[i])
+
+    }
+    return result
+
+}
+console.log(first([7, 9, 0, -2],6));
