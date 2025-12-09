2f3c755e5ade1d1d10f772befc2a51df0b42a782
Annetaliya <annetaliya@gmail.com>
2025-12-09 14:21:44 +0300

    find array leaders

---
+++ challenge98.js
@@ -0,0 +1,31 @@
+/**
+ * Given an array arr[] of size n, the task is to find all the Leaders in the array. 
+ * An element is a Leader if it is greater than or equal to all 
+ * the elements to its right side.
+
+Note: The rightmost element is always a leader.
+Input: arr[] = [16, 17, 4, 3, 5, 2]
+Output: [17 5 2]
+Explanation: 17 is greater than all the elements to its right 
+i.e., [4, 3, 5, 2], therefore 17 is a leader. 
+5 is greater than all the elements to its right i.e., 
+[2], therefore 5 is a leader. 2 has no element to its right, therefore 2 is a leader.
+ */
+
+function arrayLeader(arr) {
+    let result = [];
+    const n = arr.length;
+    let end = arr[n -1]
+
+    for (let i = 0; i < n; i++){
+        for (let j = i + 1; j < n; j++) {
+            if (arr[i] >= arr[j]) {
+                if (!result.includes(arr[i])) {
+                    result.push(arr[i])
+                }
+            }
+        }
+    }
+    return result
+}
+console.log(arrayLeader([16, 17, 4, 3, 5, 2]))
